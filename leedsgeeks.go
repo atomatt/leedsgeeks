b98f7a721c937d37ad5e53866373256c3a721917
Matt Goodall <matt.goodall@gmail.com>
2013-11-04 12:45:10 +0000

    Render something from the config file.

--- leedsgeeks.go
+++ leedsgeeks.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 var (
-	port = flag.String("port", "5000", "http port")
+	configFile = "leedsgeeks.json"
+	port       = flag.String("port", "5000", "http port")
 )
+
+type Config struct {
+	Groups []Group
+}
+
+type Group struct {
+	Name string
+}
 
 func main() {
 	flag.Parse()
@@ -17,5 +28,27 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!\n")
+	config, err := readConfig()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	fmt.Fprintf(w, config.Groups[0].Name)
+}
+
+func readConfig() (*Config, error) {
+
+	f, err := os.Open(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("%s not found", configFile)
+	}
+	defer f.Close()
+
+	config := &Config{}
+	err = json.NewDecoder(f).Decode(config)
+	if err != nil {
+		return nil, fmt.Errorf("%s is invalid", configFile)
+	}
+
+	return config, nil
 }
